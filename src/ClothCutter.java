9ad18b6188dc2121e5bab0fb6434dc786469c683
Jeff <zip.github@goldsy.net>
2012-09-16 11:39:44 -0400

    Initial implementation

---
+++ src/ClothCutter.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList ;
+
+
+/**
+ * Class to initiate the cloth cutting optimization process.
+ * 
+ * @author Jeff
+ *
+ */
+public class ClothCutter {
+	/**
+	 * Class ctor.
+	 * 
+	 * @param width
+	 * 		Width of the cloth.
+	 * @param height
+	 * 		Height of the cloth.
+	 * @param patterns
+	 * 		List of patterns to be placed on the cloth.
+	 */
+	public ClothCutter(int width, int height, ArrayList<Patterns> patterns) {
+		// TODO: (goldsy) FINISH me.
+	}
+}
+
+
+/**
+ * Initiates the optimize operation.
+ */
+public void optimize() {
+	// TODO: (goldsy) Finish me.
+}
